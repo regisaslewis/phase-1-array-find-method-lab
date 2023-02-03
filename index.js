fa7df649b920e289bb28d0289daf42bf6ddd91d3
RegisLewis <regisaslewis@gmail.com>
2023-02-02 20:35:49 -0800

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function isWin(item) {
+    return item.result === "W";
+  }
+  
+  function superbowlWin(array) {
+  let win = array.find(isWin);
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+  }
